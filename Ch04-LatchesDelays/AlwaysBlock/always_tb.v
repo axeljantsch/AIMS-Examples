a7319093e73e265d4bc9dfe9e10fa947b2968eb4
Axel Jantsch <axel.jantsch@tuwien.ac.at>
2022-07-28 09:33:09 +0200

    AJ: Update of Makefiles in Always part in chapter 4.

--- Ch04-LatchesDelays/AlwaysBlock/always_tb.v
+++ Ch04-LatchesDelays/AlwaysBlock/always_tb.v
@@ -1,12 +1,11 @@
-
+ 
 `timescale 100ps/10ps
 
 module alwtb ();
    reg d;
    reg clk = 1'b0;
    wire qcomb, qseq, qcont;
-
-
+   
    alwcomb al1 (.d(d), .q(qcomb));
    alwcont al3 (.d(d), .q(qcont));
    alwseq  al2 (.d(d), .clk(clk), .q(qseq));
@@ -14,7 +13,7 @@ module alwtb ();
    always #10 clk = ~clk;
       
    initial begin
-      $dumpfile("always.vcd");
+      $dumpfile("always_tb.vcd");
       $dumpvars(0,alwtb);
 
       #1 d = 1'b0;
