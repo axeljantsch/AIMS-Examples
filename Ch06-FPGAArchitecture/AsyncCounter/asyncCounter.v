c54bfd4bc68ec5dc2a189d00cbb5e71ff59f1115
Axel Jantsch <axel.jantsch@tuwien.ac.at>
2022-08-24 16:28:58 +0200

    Update of Makefile for AsyncCounter

--- Ch06-FPGAArchitecture/AsyncCounter/asyncCounter.v
+++ Ch06-FPGAArchitecture/AsyncCounter/asyncCounter.v
@@ -1,25 +1,29 @@
 
 `timescale 100ps/10ps
 
-module asyncCounter(clk,q);
-   input clk;
+module asyncCounter(clk,rst,q);
+   input clk, rst;
    output wire [3:0] q;
-   
-   wire clk;
-
-   dff a1(!q[0],clk,q[0]);
-   dff a2(!q[1],!q[0],q[1]);
-   dff a3(!q[2],!q[1],q[2]);
-   dff a4(!q[3],!q[2],q[3]);
+   wire [3:0] 	     qn;
+      
+   dff a1(qn[0],clk,  rst, q[0], qn[0]);
+   dff a2(qn[1],qn[0],rst, q[1], qn[1]);
+   dff a3(qn[2],qn[1],rst, q[2], qn[2]);
+   dff a4(qn[3],qn[2],rst, q[3], qn[3]);
 endmodule
 
-module dff(d,clk,q);
-   input d, clk;
+module dff(d,clk,rst,q,qn);
+   input d, rst, clk;
    output reg q;
-   initial
-     q = 1'b1;
-   always @(posedge clk) begin
-      q <= #1 d;
+   output wire qn;
+
+   always @(posedge clk or posedge rst) begin
+      if (rst)
+	q <= #1 1'b0;
+      else
+	q <= #1 d;
    end
+   assign qn = ~q;
+   
 endmodule
 
---
+++ Ch06-FPGAArchitecture/AsyncCounter/setup.tcl
@@ -0,0 +1,32 @@
+set nfacs [ gtkwave::getNumFacs ]
+set dumpname [ gtkwave::getDumpFileName ]
+set dmt [ gtkwave::getDumpType ]
+
+puts "number of signals in dumpfile '$dumpname' of type $dmt: $nfacs"
+
+set siglist [list]
+lappend siglist "clk"
+lappend siglist "rst"
+lappend siglist "q"
+
+set num_added [ gtkwave::addSignalsFromList $siglist ]
+
+
+# gtkwave::/Edit/Insert_Comment "Sender"
+
+#
+# Highlighting a particular trace:
+#
+# gtkwave::setTraceHighlightFromNameMatch BusData on
+
+gtkwave::/Time/Zoom/Zoom_Full
+# gtkwave::setWindowStartTime 100ns
+# gtkwave::setZoomRangeTimes 80ns 300ns
+
+
+# gtkwave::/File/Grab_To_File "simwave.png"
+# gtkwave::/File/Print_To_File PDF {A4 (11.68" x 8.26")} Minimal $dumpname.pdf
+
+
+#gtkwave::/File/Quit
+
