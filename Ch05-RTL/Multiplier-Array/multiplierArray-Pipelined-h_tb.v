dfc572a5c36b5ad4cd12d4b0fb25a1b082d0f9b4
Axel Jantsch <axel.jantsch@tuwien.ac.at>
2022-08-25 10:35:25 +0200

    Update of Make file and re-run of result.

---
+++ Ch05-RTL/Multiplier-Array/multiplierArray-Pipelined-h_tb.v
@@ -0,0 +1,116 @@
+
+module multiplierArray_tb;
+
+   parameter width = 64;
+   
+   reg [width-1:0] a, b;
+   wire [2*width-1:0] y;
+   reg [2*width-1:0]  yfull;
+   reg [2*width-1:0]  yprev[width-1:0];
+   
+   
+   reg 	       clk;
+   
+   
+   multiplierArray #(
+		.width(width)
+		) uut (
+		       .a(a),
+		       .b(b),
+		       .y(y),
+		       .clk(clk)
+		       ); 
+
+   reg [width-1:0]  xrnd = 11;
+   task xorshift32;
+      begin
+	 xrnd = xrnd ^ (xrnd << (width*3/3)-1);
+	 xrnd = xrnd ^ (xrnd >> (width/2)+1);
+	 xrnd = xrnd ^ (xrnd << (width*3/4)-1);
+	 xrnd = xrnd ^ (xrnd >> (width*3/3)-1);
+      end endtask
+
+   integer i,j;
+
+   always #5 clk = ~clk;
+   
+   initial begin
+      $dumpfile("multiplierArray-Pipelined-h_tb.vcd");
+      // $dumpvars(1,multiplierArray_tb.uut.preg[0]);
+      // $dumpvars(1,multiplierArray_tb.uut.preg[1]);
+      // $dumpvars(1,multiplierArray_tb.uut.preg[2]);
+      // $dumpvars(1,multiplierArray_tb.uut.preg[3]);
+      // $dumpvars(1,multiplierArray_tb.uut.partialsa[3]);
+
+      // $dumpvars(1,multiplierArray_tb.uut.partials[0]);
+      // $dumpvars(1,multiplierArray_tb.uut.partials[1]);
+      // $dumpvars(1,multiplierArray_tb.uut.partials[2]);
+      // $dumpvars(1,multiplierArray_tb.uut.partials[3]);
+
+      // $dumpvars(1,multiplierArray_tb.uut.partialss[0]);
+      // $dumpvars(1,multiplierArray_tb.uut.partialss[1]);
+      // $dumpvars(1,multiplierArray_tb.uut.partialss[2]);    
+      // $dumpvars(1,multiplierArray_tb.uut.partialss[3]);
+
+      // $dumpvars(1,multiplierArray_tb.uut.partialsco[0]);
+      // $dumpvars(1,multiplierArray_tb.uut.partialsco[1]);
+      // $dumpvars(1,multiplierArray_tb.uut.partialsco[2]);
+      // $dumpvars(1,multiplierArray_tb.uut.partialsco[3]);
+      // $dumpvars(1,multiplierArray_tb.uut.preg[3]);
+      // $dumpvars(1,multiplierArray_tb.uut.preg[5]);
+      // $dumpvars(1,multiplierArray_tb.uut.preg[7]);
+      $dumpvars(0, multiplierArray_tb);
+ 
+      clk = 1'b0;
+      
+      for (i = 0; i < 20; i = i+1)
+	begin
+	   #2;
+
+	   xorshift32;
+	   a <= xrnd;
+	   xorshift32;
+	   b <= xrnd;
+
+	   // if (i == 0) begin
+	   //    a <= 4'b1101;
+	   //    b <= 1;
+	   // end
+	   // else if (i == 1) begin
+	   //    a <= 4'b1001;
+	   //    b <= i;
+	   // end
+	   // else begin
+	   //    a <= 4'b1111;
+	   //    b <= i;
+	   // end
+	   	   
+	   #1;
+	   
+	   yfull = a*b;
+	   #7;
+
+	   $display("%d * %d = %d (%d, Prev 1: %d)", a, b, y, yfull, yprev[1]);
+	   if (y != yprev[1]) begin
+	      $display("ERROR!");
+	      // $finish;
+	   end
+	    for (j=width-1; j>0; j=j-1) begin
+	      yprev[j] = yprev[j-1];
+	   end
+	   yprev[0] = yfull;
+	   
+	end // for (i = 0; i < 100; i = i+1)
+
+      a = 8'b11111111;
+      b = 8'b11111111;
+
+      #5;
+      
+      $display("PASSED.");
+      $finish;
+      
+   end
+
+endmodule
+
---
+++ Ch05-RTL/Multiplier-CarrySave/multiplierCarry-Pipelined-0_tb.v
@@ -0,0 +1,101 @@
+
+module multiplierCarry_tb;
+
+   parameter width = 16;
+   
+   reg [width-1:0] a, b;
+   wire [2*width-1:0] y;
+   reg [2*width-1:0]  yfull, yprev;
+   
+   
+   reg 	       clk;
+   
+   
+   multiplierCarry #(
+		.width(width)
+		) uut (
+		       .a(a),
+		       .b(b),
+		       .y(y),
+		       .clk(clk)
+		       ); 
+
+   reg [width-1:0]  xrnd = 9;
+   task xorshift32;
+      begin
+	 xrnd = xrnd ^ (xrnd << (width*3/12));
+	 xrnd = xrnd ^ (xrnd >> (width/2));
+	 xrnd = xrnd ^ (xrnd << (width*3/4));
+	 xrnd = xrnd ^ (xrnd >> (width*3/12));
+	 xrnd = xrnd ^ (xrnd >> (3));
+	 xrnd = xrnd ^ (xrnd << (13));
+      end endtask
+
+   integer i;
+
+   always #5 clk = ~clk;
+   
+   initial begin
+      $dumpfile("multiplierCarry-Pipelined-0_tb.vcd");
+      $dumpvars(1,multiplierCarry_tb.uut.partialsa[0]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsa[1]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsa[2]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsa[3]);
+
+      $dumpvars(1,multiplierCarry_tb.uut.partialsb[0]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsb[1]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsb[2]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsb[3]);
+
+      $dumpvars(1,multiplierCarry_tb.uut.partialss[0]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialss[1]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialss[2]);    
+      $dumpvars(1,multiplierCarry_tb.uut.partialss[3]);
+
+      $dumpvars(1,multiplierCarry_tb.uut.partialsco[0]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsco[1]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsco[2]);
+      $dumpvars(1,multiplierCarry_tb.uut.partialsco[3]);
+      // $dumpvars(1,multiplierCarry_tb.uut.preg[3]);
+      // $dumpvars(1,multiplierCarry_tb.uut.preg[5]);
+      // $dumpvars(1,multiplierCarry_tb.uut.preg[7]);
+      $dumpvars(0, multiplierCarry_tb);
+ 
+      clk = 1'b0;
+      
+      for (i = 0; i < 100; i = i+1)
+	begin
+	   #1;
+
+	   xorshift32;
+	   a <= xrnd;
+	   xorshift32;
+	   b <= xrnd;
+
+	   #1;
+	   
+	   yfull = a*b;
+	   #8;
+
+	   $display("%d * %d = %d (%d; %d)", a, b, y, yfull, yprev);
+	   if (yprev != y) begin
+	      $display("ERROR!");
+	      $finish;
+	   end
+
+	   yprev = yfull;
+	   
+	end // for (i = 0; i < 100; i = i+1)
+
+      a = 8'b11111111;
+      b = 8'b11111111;
+
+      #5;
+      
+      $display("PASSED.");
+      $finish;
+      
+   end
+
+endmodule
+
